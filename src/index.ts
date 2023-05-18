4b00c44316f078f680320be667c7bbe356671fec
lehaiquantb <lehaiquantb@gmail.com>
2023-05-19 01:33:28 +0700

    Chore: up

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,3 @@ import moment from 'moment';
 
 console.log('HELLO POSTMAN', moment().format('YYYY-MM-DD'));
 const M = moment
-// var _Faker = {
-//     email: function () {
-//         pm.variables.replaceIn('{{$randomEmail}}');
-//     },
-// };
-
-// // @ts-ignore
-// Faker = _Faker;
